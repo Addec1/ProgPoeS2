cfdb0deb863fe1f5a5fd5009754e342348d9e42f
lab_services_student <lab_services_student@lab7L95SR>
2025-10-22 14:49:14 +0200

    fix(claims): format hours and amounts (R N2) and right-align numeric columns

--- obj/Debug/net8.0/CMCS.Prototype.AssemblyInfo.cs
+++ obj/Debug/net8.0/CMCS.Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS.Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe0f5e89b20f9ac102fd476c99d7197d832a527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f826ce1c2664883b9a50ec4b40919ff0974c9da")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS.Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS.Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
